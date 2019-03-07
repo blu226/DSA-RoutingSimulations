0a44f06b585c8ab6e17603221ceb98cb5f55e3e9
Vijay <vjsah27@gmail.com>
2019-03-06 23:34:09 -0500

    new sim 2

--- run_simulation.py
+++ run_simulation.py
@@ -234,7 +234,7 @@ else:
             #     run_various_sims(sim_round, num_mules, num_channels, num_Pusers, msg_round, msg_mean, ttl, mem_size,
             #                      num_replicas)
 
-            for num_mules in [128, 16, 32, 64, 84]:
+            for num_mules in [84, 16, 32, 64]:
                 print("Msg round", msg_round, "Mules", num_mules)
                 run_various_sims(sim_round, num_mules, num_channels, num_Pusers, msg_round, msg_mean, ttl, mem_size,
                                  num_replicas)
